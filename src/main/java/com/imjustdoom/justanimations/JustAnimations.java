c9e751f26d8416bab590af6dcda2426cfb9c2b62
JustDoom <61824552+JustDoom@users.noreply.github.com>
2022-04-04 13:54:15 +1000

    Add bstats

--- src/main/java/com/imjustdoom/justanimations/JustAnimations.java
+++ src/main/java/com/imjustdoom/justanimations/JustAnimations.java
@@ -4,6 +4,8 @@ import com.imjustdoom.justanimations.command.AnimationsCommand;
 import com.imjustdoom.justanimations.command.tabcomplete.AnimationsTabCompletion;
 import com.imjustdoom.justanimations.config.AnimationsConfig;
 import lombok.Getter;
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SingleLineChart;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -22,6 +24,12 @@ public final class JustAnimations extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        int pluginId = 14842;
+        Metrics metrics = new Metrics(this, pluginId);
+
+        // (This is useless as there is already a player chart by default.)
+        metrics.addCustomChart(new SingleLineChart("animations", animations::size));
 
         saveDefaultConfig();
 
